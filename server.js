61576a0e64062f12c9aab917653f02f35b866e29
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-20 18:55:06 -0500

    added delete and added candidates roudte

--- server.js
+++ server.js
@@ -23,13 +23,22 @@ const db = new sqlite3.Database('./db/election.db', err => {
 //     console.log(rows);
 //   });
 
-// GET a single candidate
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if(err) {
-//       console.log(err);
-//     }
-//     console.log(row);
-//   });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
   // Create a candidate
   const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
@@ -44,17 +53,21 @@ console.log(result, this.lastID);
 });
 
 
-  // Delete a candidate
-db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result, this, this.changes);
-  });
-
-  // Default response for any other request(Not Found) Catch all
-app.use((req, res) => {
-    res.status(404).end();
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
   });
 
 
