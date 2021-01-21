5e633614368f0a0d3c77e2656ab1ec564c8ceb01
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-21 10:08:22 -0500

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,10 @@
+CREATE TABLE parties (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
@@ -5,8 +12,3 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL
 );
 
-CREATE TABLE parties (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
