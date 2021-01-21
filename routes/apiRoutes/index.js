a64bc655cf9c98e46c2af8c1c91e2399ea2a62c1
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-21 18:35:43 -0500

    added and upded routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./candidateRoutes'));
+router.use(require('./voterRoutes'));
+router.use(require('./partyRoutes'));
 
 module.exports = router;
--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -1,56 +1,52 @@
-router.use(require('./partyRoutes'));
-
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/database');
 
+// Get all parties
+router.get('/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
 
-
-router.get('/api/parties', (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    const params = [];
-    db.all(sql, params, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: rows
-      });
+    res.json({
+      message: 'success',
+      data: rows
     });
   });
+});
 
+// Get single party
+router.get('/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
 
-  router.get('/api/party/:id', (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.get(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-  
-      res.json({
-        message: 'success',
-        data: row
-      });
+    res.json({
+      message: 'success',
+      data: rows
     });
   });
+});
 
+// Delete a party
+router.delete('/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`
+  db.run(sql, req.params.id, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
 
-  router.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.run(sql, params, function(err, result) {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        return;
-      }
-  
-      res.json({ message: 'successfully deleted', changes: this.changes });
-    });
+    res.json({ message: 'deleted', changes: this.changes });
   });
+});
 
-  module.exports = router;
+module.exports = router;
